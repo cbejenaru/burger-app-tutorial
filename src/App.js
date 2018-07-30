acf4f350f046383b6354967a3af0d316b7a03e56
Constantin Bejenaru <cbejenaru@starlab.md>
2018-07-30 12:25:12 +0300

    Create burgerIngredint.Add prop validation for burgerIngerdient

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurberBuilder/BurgerBuilder';
+import Burger from './components/Burger/Burger';
 import './App.css';
 
 class App extends Component {
